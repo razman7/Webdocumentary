af89d565a02a55bef8d55c63a6dc3a1a9273256c
Matija Hrnjak <hrnjak.matija@gmail.com>
2019-02-19 21:30:09 +0100

    cawde

--- code/js/sscript.js
+++ code/js/sscript.js
@@ -64,7 +64,7 @@ animateHTML().init();
 
 window.onload = function() { //executes this code after the DOM loads
    //--- this is the selector element. Feel free to change this if you don't want all video objects targeted.
-  const vids = document.getElementsById(`hover`)
+  const vids = document.querySelectorAll(`.hover`)
 
    //--- Now we loop over all of the selected elements and add event listeners
   for (let i = 0; i < vids.length; i++) {
