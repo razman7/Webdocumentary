3655b13db135cb512a1119fc24cc0afad126be28
Matija Hrnjak <hrnjak.matija@gmail.com>
2019-02-19 17:58:08 +0100

    gnidoc

--- code/js/sscript.js
+++ code/js/sscript.js
@@ -59,3 +59,38 @@ var animateHTML = function() {
   };
 };
 animateHTML().init();
+
+
+
+window.onload = function() { //executes this code after the DOM loads
+   //--- this is the selector element. Feel free to change this if you don't want all video objects targeted.
+  const vids = document.getElementsById(`hover`)
+
+   //--- Now we loop over all of the selected elements and add event listeners
+  for (let i = 0; i < vids.length; i++) {
+    vids[i].addEventListener( `mouseover`, function(e) {
+      vids[i].play()
+    })
+    vids[i].addEventListener( `mouseout`, function(e) {
+      vids[i].pause()
+    })
+  }
+}
+
+var v = document.getElementByTagName('video');
+v.addEventListener(
+    'play',
+    function() {
+        v.play();
+    },
+    false);
+
+v.onclick = function() {
+    if (v.paused) {
+        v.play();
+    } else {
+        v.pause();
+    }
+
+    return false;
+};
